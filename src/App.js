25f5266be0ecf15614cc7b609d23cf99f9def39c
happy-7898 <singhharpreet5409@gmail.com>
2024-06-19 12:36:45 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import search from './assets/icons/search.svg'
+import { useStateContext } from './Context/ind.js';
+import BackgroundLayout from './components/BackgroundLayout';
+import WeatherCard from './components/WeatherCard';
+import MiniCard from './components/MiniCard';
 
 function App() {
+
+  const [input,setInput]=useState('');
+  const {weather,thisLocation,values,place,setPlace}=useStateContext();
+  // console.log(weather);
+
+  const submitCity=()=>{
+    setPlace(input)
+    setInput('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+        <nav className='navbar'>
+          <h1 className='mainHeading'>Weather App</h1>
+
+          <div className='inputBox'>
+            <img src={search} alt='search' className='searchimage'></img>
+            <input type='text' onKeyUp={(e)=>{ 
+              if(e.key === 'Enter'){
+                submitCity()
+              }
+              }} className='inputField1' placeholder='Search City' value={input} onChange={e=>setInput(e.target.value)}></input>
+          </div>
+        </nav>
+        <BackgroundLayout></BackgroundLayout>
+        <main className='mainContainer'>
+             < WeatherCard 
+              place={thisLocation} 
+              windspeed={weather.wspd}
+              humidity={weather.humidity}
+              temperature={weather.temp}
+              heatIndex={weather.heatindex}
+              iconString={weather.conditions}
+              conditions={weather.conditions}
+
+             ></WeatherCard>
+
+            <div className='mini-card'>
+              {
+                values?.slice(1,7).map(curr=>{
+                  return (<MiniCard
+                    key={curr.datetime}
+                    time={curr.datetime}
+                    temp={curr.temp}
+                    iconString={curr.conditions}
+                  >
+
+                  </MiniCard>)
+                })
+              }
+            </div>
+        </main>
+
+        
     </div>
   );
 }
